3bc8d858e7000020290728a90b475afd51038408
jenjohmar <69906853+jenjohmar@users.noreply.github.com>
2021-06-11 12:00:58 +0200

    upload 3 June 11th

--- main.py
+++ main.py
@@ -25,18 +25,24 @@ def create_csv(name_csv, header):
             csv_writer.writeheader()
 
 # writes the data from the dictionary passed in through the buy function, to bought.csv 
-# TO DO: convert the "amount" to the same number of rows in bought.csv
+# writes the "amount" from the buy function to the same number of rows in bought.csv
+# TO DO: ids must increment by 1 (instead of all the same ids)
 
-def write_to_bought(bought_dict): # new_product_dict from buy function gets passed in
-    with open('bought.csv', 'a', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=header_bought)
-        csv_writer.writerow(bought_dict)
+def write_to_bought(bought_dict, amount): # new_product_dict and amount from buy function get passed in
+    for i in range(amount):
+        with open('bought.csv', 'a', newline='') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=header_bought)
+            csv_writer.writerow(bought_dict)
 
-# writes the data from the dictionary passed in through the buy function, to inventory.csv 
-def write_to_inventory(bought_dict): # product_dict_inv from buy function gets passed in
-    with open('inventory.csv', 'a', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=header_inventory)
-        csv_writer.writerow(bought_dict)
+# writes the data from the dictionary passed in through the buy function, to inventory.csv
+# writes the "amount" from the buy function to the same number of rows in inventory.csv
+# TO DO: ids must increment by 1 (instead of all the same ids)
+
+def write_to_inventory(bought_dict, amount): # product_dict_inv from buy function gets passed in
+    for i in range(amount):
+        with open('inventory.csv', 'a', newline='') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=header_inventory)
+            csv_writer.writerow(bought_dict)
 
 # writes the data from the dictionary passed in through the sell function, to sold.csv
 # TO DO: convert the "amount" to the same number of rows in sold.csv
@@ -60,19 +66,18 @@ def buy(prod_name: str, buy_price: float, buy_date: str, exp_date: str, amount: 
         "buy_price": buy_price,
         "buy_date": buy_date,
         "exp_date": exp_date,
-        
     }
 
-    write_to_bought(new_product_dict)
+    write_to_bought(new_product_dict, amount)
 
     product_dict_inv = {
         "product_id": id,
         "product_name": prod_name,
         "exp_date": exp_date,
-        
+   
     }
     
-    write_to_inventory(product_dict_inv)
+    write_to_inventory(product_dict_inv, amount)
 
 def sell(prod_name: str, sell_date: str, sell_price, amount):
     number_of_lines = 0
@@ -118,24 +123,20 @@ def sort_inventory_exp_date():
 
     return inventory
     
-    
-
-
-        
-
+  
 # run code    
 def main():
-    # create_csv("bought.csv", header_bought)
-    # create_csv("inventory.csv", header_inventory)
-    # create_csv("sold.csv", header_sold)
+    create_csv("bought.csv", header_bought)
+    create_csv("inventory.csv", header_inventory)
+    #create_csv("sold.csv", header_sold)
     
-    # buy("shampoobar", 2.32, "2021-02-02", "2025-03-02", 50)
+    buy("shampoobar", 2.32, "2021-02-02", "2025-03-02", 5)
     # buy("peanut", 0.03, "2021-01-15", "2030-03-02", 500)
     # buy("apple", 0.87, "2021-06-11", "2021-06-25", 100)
     # sell("apple", "2021-11-11", 2.00, 5)
 
     #print(read_csv())
-    sort_inventory_exp_date()
+    #sort_inventory_exp_date()
 
 if __name__ == '__main__':
     main()
